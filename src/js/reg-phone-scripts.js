a083ee4a3f7514812e0fe2219ac4859dfc5732bb
Hasith Yaggahavita <hasith@gmail.com>
2017-04-02 18:53:21 +0530

    tunning the flow

--- src/js/reg-phone-scripts.js
+++ src/js/reg-phone-scripts.js
@@ -1,6 +1,30 @@
 
 $(document).ready(function(){
+    
+    //countrycode input setup
+    $("#phone").intlTelInput({
+      // allowDropdown: false,
+      // autoHideDialCode: false,
+      // autoPlaceholder: "off",
+      // dropdownContainer: "body",
+      // excludeCountries: ["us"],
+      // formatOnDisplay: false,
+      // geoIpLookup: function(callback) {
+      //   $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+      //     var countryCode = (resp && resp.country) ? resp.country : "";
+      //     callback(countryCode);
+      //   });
+      // },
+      // initialCountry: "auto",
+      // nationalMode: false,
+      // onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
+      // placeholderNumberType: "MOBILE",
+      // preferredCountries: ['cn', 'jp'],
+      // separateDialCode: true,
+      utilsScript: "js/utils.js"
+    });
 
+    
     var API_URL = "https://oxe44imldk.execute-api.us-west-2.amazonaws.com/dev/register";
     $( "#phoneForm" ).submit(function( event ) {
         //--
