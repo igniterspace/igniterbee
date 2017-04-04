6073ae1b3dd87ca2bf174e30d88cf06afdc3eed3
nimaz0 <nimaz@igniterspace.com>
2017-04-04 21:04:50 +0530

    Removed the phone number from URL for reg-verify

--- src/js/reg-phone-scripts.js
+++ src/js/reg-phone-scripts.js
@@ -43,7 +43,9 @@ $(document).ready(function(){
                     $("#dialog-error").dialog('option', 'title', "Registration failed!");
                     $("#dialog-error").dialog('open');
                 } else {
-                    location.href = "./reg-verify.html" + "?phone=" + phoneNumber + "&tab=" + tab;
+                    //location.href = "./reg-verify.html" + "?phone=" + phoneNumber + "&tab=" + tab;
+                    location.href = "./reg-verify.html" + "?tab=" + tab;
+
                 }
             }
         });
