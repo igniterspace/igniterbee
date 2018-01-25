b711121196e96c30c749ba0051b2fdf721f73ee5
Hasith Yaggahavita <hasith@gmail.com>
2018-01-25 11:12:48 +0530

    deployed to AWS

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 ng build
 aws s3 sync dist/ s3://lab.igniterbee.com --region ap-south-1 --delete
-aws cloudfront create-invalidation --distribution-id E2KRBDFE2FFI8P \ --paths /*
+aws cloudfront create-invalidation --distribution-id E2KRBDFE2FFI8P --paths /*
 git commit -am "deployed to AWS"
 git push
